2525853ae274d884298a66018bfc612d0334acc9
glassyys <yys9289@gmail.com>
2026-01-09 15:25:26 +0900

    구구단 프로그램 추가

---
+++ multiplication_table.py
@@ -0,0 +1,14 @@
+﻿def print_multiplication_table(num):
+    """구구단 출력"""
+    print(f"\n===== {num}단 =====")
+    for i in range(1, 10):
+        result = num * i
+        print(f"{num}  {i} = {result}")
+
+def main():
+    """메인 함수"""
+    for num in range(2, 10):
+        print_multiplication_table(num)
+
+if __name__ == "__main__":
+    main()
